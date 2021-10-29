4a940747d73593a2fd240e31d6fae804e945c3c2
banard33-233 <banard33@163.com>
2021-10-29 16:10:07 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # emotional-tendency
+武汉大学国家网络安全学院-社会计算 课程项目1
---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding=utf-8 -*-
+import csv
+
+data_location='./外卖评论.csv'
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
